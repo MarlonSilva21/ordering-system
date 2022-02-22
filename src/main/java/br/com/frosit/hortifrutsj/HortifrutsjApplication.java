ed1df4cb855cb8292f91265175e23833456211c4
MarlonSilva21 <josemarlondasilva48@gmail.com>
2022-02-22 17:12:32 -0300

    Projeto criado

---
+++ src/main/java/br/com/frosit/hortifrutsj/HortifrutsjApplication.java
@@ -0,0 +1,13 @@
+package br.com.frosit.hortifrutsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortifrutsjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HortifrutsjApplication.class, args);
+    }
+
+}
